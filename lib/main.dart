e7a52cf0461aded311491d6458fc732712b9616f
pedrofeld <pedro.gael.feld@gmail.com>
2026-09-25 21:05:58 -0300

    feat: transform home in an async function that accepts .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'Login.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Login(),
-    ),
-  );
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MaterialApp(home: Login()));
 }

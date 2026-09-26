fcf19b0c0d24b31cc90e5393cccccbf4e12534e7
pedrofeld <pedro.gael.feld@gmail.com>
2026-09-26 08:31:08 -0300

    chore: improve architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
-import 'pages/Login.dart';
+import 'pages/Login_page.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'service_response.dart';
+import '../config/service_response.dart';
 
 class LoginService {
   LoginService({http.Client? client}) : _client = client ?? http.Client();
